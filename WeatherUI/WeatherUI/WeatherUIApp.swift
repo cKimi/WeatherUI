d283277ada8d9eea0477bc844aaa0fc2b6450278
Carlos Kimura <ckimura@gmail.com>
2023-12-06 17:24:56 -0300

    initial commit

---
+++ WeatherUI/WeatherUI/WeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIApp.swift
+//  WeatherUI
+//
+//  Created by Carlos Kimura on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
